fbcea45c0bb87861bf3d135f69a65693f9769ee6
rsk94zoo <rachid.abdoulalime@gmail.com>
2023-10-11 22:23:52 +0200

    Correction bug classe

--- SpaceInvaders/Vecteur2D.cs
+++ SpaceInvaders/Vecteur2D.cs
@@ -2,7 +2,7 @@
 
 namespace SpaceInvaders
 {
-    public class Vecteur2D
+    class Vecteur2D
     {
         private double x;
         private double y;
@@ -12,6 +12,10 @@ namespace SpaceInvaders
             this.x = x;
             this.y = y;
         }
+
+        public double X() { return x; }
+
+        public double Y() { return y; }
 
         public double Norme
         {
@@ -25,28 +29,38 @@ namespace SpaceInvaders
         {
             return new Vecteur2D(v1.x + v2.x, v1.y + v2.y);
         }
+
+
         public static Vecteur2D operator -(Vecteur2D v1, Vecteur2D v2)
         {
             return new Vecteur2D(v1.x - v2.x, v1.y - v2.y);
         }
+
 
         public static Vecteur2D MoinsUnaire(Vecteur2D v1)
         {
             return new Vecteur2D(-v1.x, -v1.y);
         }
 
+
         public static Vecteur2D operator *(Vecteur2D v1, double scalaireDroite)
         {
             return new Vecteur2D(v1.x * scalaireDroite, v1.y * scalaireDroite);
         }
+
+
         public static Vecteur2D operator *(double scalaireGauche, Vecteur2D v1)
         {
             return new Vecteur2D(scalaireGauche * v1.x , scalaireGauche * v1.y);
         }
 
+
         public static Vecteur2D operator /(Vecteur2D v1, double scalaire)
         {
             return new Vecteur2D( v1.x / scalaire, v1.y / scalaire);
         }
+
+
     }
+
 }
