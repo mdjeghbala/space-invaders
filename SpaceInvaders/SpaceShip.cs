16e249ce259bfc0dab6b59934384e2596f29342a
mdjeghbala <mohamed.djeghbala@outlook.fr>
2023-10-29 13:34:51 +0100

    Correction du déplacement du vaisseau joueur dans la méthode Update de SpaceShip.cs qui avait été supprimé

--- SpaceInvaders/SpaceShip.cs
+++ SpaceInvaders/SpaceShip.cs
@@ -29,7 +29,32 @@ namespace SpaceInvaders
 
         public override void Update(Game gameInstance, double deltaT)
         {
+            // DEPLACEMENT LATERAL
+            if (gameInstance.keyPressed.Contains(Keys.Left))
+            {
+                base.Position.x -= speedPixelPerSecond * deltaT;
+                Console.WriteLine("Gauche : " + base.Position.X);
+            }
+            if (gameInstance.keyPressed.Contains(Keys.Right))
+            {
+                base.Position.x += speedPixelPerSecond * deltaT;
+                Console.WriteLine("Droite : " + base.Position.X);
+            }
 
+            // CONTROLE LA SORTIE HORS DE LA ZONE DE JEU HORIZONTALE
+            if (base.Position.X > gameInstance.gameSize.Width - 25)
+            {
+                base.Position.x = (gameInstance.gameSize.Width) - 25;
+            }
+            if (base.Position.X < 0)
+            {
+                base.Position.x = 0;
+            }
+
+            if (gameInstance.keyPressed.Contains(Keys.Up))
+            {
+                this.shoot(gameInstance);
+            }
         }
 
 
